889ff353d4ecb16ced8fc386feaff692c81e369f
swapinga <galgaztin@gmail.com>
2024-01-21 18:16:29 +1100

    servidor con express

---
+++ Desafio-Servidor/src/ProductManager.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+
+class ProductManager {
+  constructor() {
+    this.autoIncrementId = 1;
+  }
+
+  addProduct(product) {
+    if (
+      !product.title ||
+      !product.description ||
+      !product.price ||
+      !product.thumbnail ||
+      !product.code ||
+      !product.stock
+    ) {
+      console.error("Todos los campos son obligatorios");
+      return;
+    }
+
+    if (fs.existsSync(`./archivos/${product.code}.txt`)) {
+      console.error("Ya existe un producto con ese código");
+      return;
+    }
+
+    const newProduct = {
+      ...product,
+        path: `./archivos/${this.autoIncrementId}`,
+        id: this.autoIncrementId++
+    };
+    console.log("Producto agregado:", newProduct);
+    fs.writeFileSync(`${newProduct.path}.txt`,`${JSON.stringify(newProduct)}`)
+  }
+
+  getProducts() {
+    const archivosPath = './archivos/';
+    const files = fs.readdirSync(archivosPath);
+  
+    const products = files
+      .map((file) => {
+        const filePath = `${archivosPath}${file}`;
+        const contenido = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+        return contenido;
+      })
+      .sort((a, b) => a.id - b.id);
+  
+    return products;
+  }
+
+  getProductById(id) {
+    if(fs.existsSync(`./archivos/${id}.txt`)){
+      let contenido = JSON.parse(fs.readFileSync(`./archivos/${id}.txt`,'utf-8'));
+      return contenido;
+    }
+    else {
+      console.log("Producto no encontrado");
+    }
+  }
+
+  updateProduct(id, contenidoNuevo) {
+    let contenidoOriginal = this.getProductById(id);
+      const productoNuevo = {
+        ...contenidoOriginal,
+        ...contenidoNuevo,
+        id: id, 
+      };
+
+      fs.writeFileSync(`${productoNuevo.path}.txt`, `${JSON.stringify(productoNuevo)}`);
+
+      console.log("Producto actualizado:", productoNuevo);
+    }
+
+    deleteProduct(id) {
+      const filePath = `./archivos/${id}.txt`;
+  
+      if (fs.existsSync(filePath)) {
+        const deletedProduct = this.getProductById(id);
+  
+        fs.unlinkSync(filePath);
+  
+        console.log("Producto eliminado:", deletedProduct);
+      } else {
+        console.error("Producto no encontrado");
+      }
+    }
+}
+
+module.exports = ProductManager;
--- Desafio-Servidor/src/app.js
+++ Desafio-Servidor/src/app.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const ProductManager = require('./ProductManager');
 
 const app = express();
+const productManager = new ProductManager();
 
-app.get('/saludo',(req,res)=>{
-    res.send("Hola a todos, pero ahora desde express")
-})
+
+app.get('/products', (req, res) => {
+    const limit = req.query.limit;
+  
+    if (limit) {
+      const limitedProducts = productManager.getProducts().slice(0, limit);
+      res.json({limitedProducts });
+    } else {
+      res.json(productManager.getProducts());
+    }
+  });
+
+  app.get('/products/:pid', (req, res) => {
+    const productId = parseInt(req.params.pid);
+    const product = productManager.getProductById(productId);
+  
+    if (product) {
+      res.json({ product: product });
+    } else {
+      res.status(404).json({ error: 'Product not found' });
+    }
+  });
+  
 
 app.listen(8080,()=>console.log("Servidor arriba en el puerto 8080!"))
